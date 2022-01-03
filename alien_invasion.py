ff25d3ffc33dc8a73d3c060c22b56721a2af4ce9
Hayden-MP <hmpowers86@gmail.com>
2022-01-02 22:14:22 -0800

    game resets after play button is pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,7 +30,7 @@ def run_game():
     # Start with main loop for the game
     while True:
         # Checks for key presses and mouse events
-        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
         if stats.game_active:
             ship.update()
--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ from bullet import Bullet
 from alien import Alien
 from time import sleep
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets): # Respond to key presses and mouse events
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets): # Respond to key presses and mouse events
     # Watch for keyboard and mouse events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,13 +15,23 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets): # Resp
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play"""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset the game statistics
+        stats.reset_stats()
         stats.game_active = True
+
+        # Empty the list of aliens and bullets
+        aliens.empty()
+        bullets.empty()
+
+        # Create a new fleet and center the ship
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
