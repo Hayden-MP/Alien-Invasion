981c88e2d7ee178973185f918a8182bf411687af
Hayden-MP <hmpowers86@gmail.com>
2022-01-06 20:06:42 -0800

    high score added

--- settings.py
+++ settings.py
@@ -51,6 +51,5 @@ class Settings():
         self.alien_speed_factor *= self.speedup_scale
 
         self.alien_points = int(self.alien_points * self.score_scale)
-        print(self.alien_points)
 
     
